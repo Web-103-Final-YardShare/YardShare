7ee2257a689aae9dee15e72d0fad27e6e0479c89
Dhruv-Dev <vasusharma2000@hotmail.com>
2025-11-16 02:46:20 -0500

    Vite update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['yardshare-frontend.onrender.com'],
+    host: true,
+  },
+  server: {
+    allowedHosts: ['yardshare-frontend.onrender.com'],
+    host: true,
+  },
 })
